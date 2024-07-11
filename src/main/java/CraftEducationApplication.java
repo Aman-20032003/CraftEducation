a8f5acc71d8dca2c2e0d9a8917ec339af7d56a82
Aman-20032003 <amankumaran2003>
2024-07-11 10:50:25 +0530

    Project commited

---
+++ src/main/java/CraftEducationApplication.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftEducationApplication.class, args);
+	}
+
+}
