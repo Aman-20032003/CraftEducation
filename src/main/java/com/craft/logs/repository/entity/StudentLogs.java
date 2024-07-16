4f0e3dd956dc6898e64ec17d7f727a4d6ddae641
AYUSH <pailossayush@gmailcom>
2024-07-16 20:12:49 +0530

    logger implemented in Student Service

--- src/main/java/com/craft/logs/repository/entity/StudentLogs.java
+++ src/main/java/com/craft/logs/repository/entity/StudentLogs.java
@@ -2,7 +2,6 @@ package com.craft.logs.repository.entity;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import lombok.Builder;
 import lombok.experimental.SuperBuilder;
 
 @Document(collection ="studentlogs")
