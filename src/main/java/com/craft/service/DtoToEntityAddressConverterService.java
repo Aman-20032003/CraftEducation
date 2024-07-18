0b303e09fe1c2b5d4802356b21c9aa003cc0031f
Aman-20032003 <amankumaran2003>
2024-07-18 12:43:24 +0530

    student registration service added

--- src/main/java/com/craft/service/DtoToEntityAddressConverterService.java
+++ src/main/java/com/craft/service/DtoToEntityAddressConverterService.java
@@ -7,20 +7,19 @@ import org.springframework.stereotype.Service;
 
 import com.craft.controller.request.GlobalAddressRequest;
 import com.craft.repository.entity.Address;
+
 @Service
 public class DtoToEntityAddressConverterService {
 
 	public Address convertToEntity(GlobalAddressRequest addressDto) {
-		Address address = new Address();
-		address.setHouseNumber(addressDto.getHouseNumber());
-		address.setCity(addressDto.getCity());
-		address.setPinCode(addressDto.getPinCode());
-		address.setState(addressDto.getState());
-		address.setCountry(addressDto.getCountry());
+		Address address = Address.builder().city(addressDto.getCity()).houseNumber(addressDto.getHouseNumber())
+				.country(addressDto.getCountry()).pinCode(addressDto.getPinCode()).State(addressDto.getState()).build();
+		
 		return address;
 	}
-	public List<Address> convertAddressListToEntity(List<GlobalAddressRequest> addressDtoList){
+
+	public List<Address> convertAddressListToEntity(List<GlobalAddressRequest> addressDtoList) {
 		return addressDtoList.stream().map(this::convertToEntity).collect(Collectors.toList());
 	}
-	
+
 }
