d55c604e32a94dc7f273a70d4b5bcb1e605a0a22
AYUSH <pailossayush@gmailcom>
2024-07-20 13:02:51 +0530

    getter setter implemented in teacher service

--- src/main/java/com/craft/controller/response/TeacherResponse.java
+++ src/main/java/com/craft/controller/response/TeacherResponse.java
@@ -2,9 +2,13 @@ package com.craft.controller.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor
+@Getter
+@Setter
 public class TeacherResponse {
 private String message;
 private int statusCode;
